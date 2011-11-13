b0bef9a29e244059ece28b5656b35c73044336e0
Michal Wojciechowski <odyniec@odyniec.net>
2011-11-13 14:31:40 +0100

    Add missing modules to build requirements

--- lib/Dancer/Plugin/Auth/Htpasswd.pm
+++ lib/Dancer/Plugin/Auth/Htpasswd.pm
@@ -15,7 +15,7 @@ use Dancer::Plugin;
 use Dancer::Response;
 use MIME::Base64;
 
-our $VERSION = '0.01';
+our $VERSION = '0.011';
 
 my $settings = plugin_setting;
 
@@ -105,7 +105,7 @@ __END__
 
 =head1 VERSION
 
-Version 0.01
+Version 0.011
 
 =head1 SYNOPSIS
 
